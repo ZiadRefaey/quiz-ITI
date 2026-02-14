f3287454a7110298f8f3feedbb99ba89b937c7cf
Ziad Refaey <ziad.refaey1@gmail.com>
2026-02-15 00:31:47 +0200

    pick an answer

--- js/Quiz/QuizApp.js
+++ js/Quiz/QuizApp.js
@@ -8,6 +8,16 @@ const nextBtn = document.querySelector(".next-btn");
 import QuizState from "./QuizStateClass.js";
 import questions from "./questionsArray.js";
 export const quiz = new QuizState(questions);
+function highlightSelected() {
+  const options = document.querySelectorAll(".option");
+  options.forEach((option, optionId) => {
+    console.log(optionId);
+    console.log(quiz.questions[quiz.currentIndex].selectedAnswer);
+    if (optionId == quiz.questions[quiz.currentIndex].selectedAnswer) {
+      option.classList.add("selected-option");
+    } else option.classList.remove("selected-option");
+  });
+}
 function renderQuestions() {
   totalQuestionsCount.textContent = quiz.questions.length;
   currentIndexDOM.textContent = quiz.currentIndex + 1;
@@ -18,7 +28,9 @@ function renderQuestions() {
   questionOptions.forEach((option, index) => {
     const optionDOM = document.createElement("div");
     optionDOM.classList.add("option");
+    optionDOM.dataset.id = index;
     optionDOM.textContent = `${index + 1}. ${option}`;
+    highlightSelected();
     if (index === 0) {
       optionsContainer.replaceChildren(optionDOM);
     } else optionsContainer.appendChild(optionDOM);
@@ -52,10 +64,23 @@ function handleNext() {
   quiz.nextQuestion();
   checkBtns();
   renderQuestions();
+  highlightSelected();
 }
 function handlePrev() {
   quiz.prevQuestion();
   checkBtns();
   renderQuestions();
+  highlightSelected();
 }
+function pickOption(questionId, optionIndex) {
+  const currentQuestion = quiz.questions[questionId];
+  console.log(currentQuestion);
+  currentQuestion.selectedAnswer = optionIndex;
+}
+optionsContainer.addEventListener("click", function (e) {
+  console.log(e.target.dataset.id);
+  pickOption(quiz.currentIndex, e.target.dataset.id);
+  highlightSelected();
+  console.log(e.target);
+});
 export default initQuiz;
