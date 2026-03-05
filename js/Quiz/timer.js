700a0e5fe12e12685db37358e358aab54eb73814
Ziad Refaey <ziad.refaey1@gmail.com>
2026-03-05 16:16:14 +0200

    timer progress bar

--- js/Quiz/timer.js
+++ js/Quiz/timer.js
@@ -1,18 +1,24 @@
 import { handleSubmit } from "./QuizApp.js";
 import Timer from "./TimerClass.js";
 const timerDOM = document.querySelector(".timer");
+const progressTimerDOM = document.querySelector(".timer-progress");
 export default function initTimer() {
   const timer = new Timer(120, onTick, onFinish);
+  progressTimerDOM.max = timer.duration;
+  console.log(progressTimerDOM.max);
+  timerDOM.textContent = convertToMinutes(120).join(":");
   function convertToMinutes(timer) {
     const minutes = Math.floor(timer / 60);
     const seconds = timer % 60;
-    return [minutes, seconds];
+    return [minutes, String(seconds).padStart(2, "0")];
   }
   function onFinish() {
     handleSubmit();
   }
   function onTick(remaining) {
     timerDOM.textContent = convertToMinutes(remaining).join(":");
+    const currentTime = timer.duration - timer.remaining;
+    progressTimerDOM.value = currentTime;
   }
   timer.start();
 }
